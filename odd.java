3ee3927c93b7fc1dbba182e4561c4a9e3bc59160
janani1316 <jananirajadurai1397@gmail.com>
2017-06-07 18:01:53 +0530

    Create odd.java

---
+++ odd.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class odd
+{
+    public static void main(String a[])
+    {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        if(n/2==0)
+        {
+            System.out.println("Number is even");
+            
+        }
+        else 
+        {
+            System.out.println("Number is odd");
+        }
+
+    }
+    
+}
+
