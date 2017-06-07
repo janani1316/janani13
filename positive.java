e7e517fac8a6b5eec06c63c38dfc10da3a317189
janani1316 <jananirajadurai1397@gmail.com>
2017-06-07 17:44:48 +0530

    Create positive.java

---
+++ positive.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class positive
+{
+    public static void main(String a[])
+    {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        if(n==0)
+        {
+            System.out.println("Number is equal to Zero");
+            
+        }
+        else if(n>0)
+        {
+            System.out.println("Number is positive");
+        }
+       else
+        {
+            System.out.println("Number is Negative");
+        }
+
+    }
+    
+}
