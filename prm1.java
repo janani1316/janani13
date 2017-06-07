1e0370e1ad937b01b3ba781e9ed8f1e8f313f755
janani1316 <jananirajadurai1397@gmail.com>
2017-06-07 18:32:50 +0530

    Create  prm1.java

---
+++ prm1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class prm1
+{
+    public static void main(String a[])
+    {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        if(n/2==0)
+        {
+            System.out.println("Number is even");
+            
+        }
+        else 
+        {
+            System.out.println("Number is odd");
+        }
+
+    }
+    
+}
