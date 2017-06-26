65468adf4000f9e7fe43b3c37e868c8907b87391
janani1316 <jananirajadurai1397@gmail.com>
2017-06-26 12:19:47 +0530

    Create palindrome.java

---
+++ palindrome.java
@@ -0,0 +1,18 @@
+class Palindrome{  
+ public static void main(String args[]){  
+ Scanner in=new Scanner(System.in);
+  int r,sum=0,temp;    
+  int n=in.nextInt(); 
+  
+  temp=n;    
+  while(n>0){    
+   r=n%10; 
+   sum=(sum*10)+r;    
+   n=n/10;    
+  }    
+  if(temp==sum)    
+   System.out.println("palindrome number ");    
+  else    
+   System.out.println("not palindrome");    
+}  
+}
