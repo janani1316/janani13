1761eae7e909a79374aac03acc93b1dd0b55182a
janani1316 <jananirajadurai1397@gmail.com>
2017-07-04 19:33:50 +0530

    Create multiplicationtable.java

---
+++ multiplicationtable.java
@@ -0,0 +1,16 @@
+    import java.util.io;
+
+    public class multiplicationtable 
+    {
+      public static void main(String[] args)
+        {
+            Scanner in= new Scanner(System.in);
+          	System.out.print("Enter number:");        
+    	      int n=in.nextInt();
+            for(int i=1; i <= 10; i++)
+            {
+             System.out.println(n+" * "+i+" = "+n*i);
+            } 
+         }
+
+    }
