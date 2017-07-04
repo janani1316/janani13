6695c222df03546f99194e5ef422560d40170367
janani1316 <jananirajadurai1397@gmail.com>
2017-07-04 19:27:36 +0530

    Update fact.java

--- fact.java
+++ fact.java
@@ -3,10 +3,10 @@ class Fact
 {
  public static void main(String args[])
  {
-  int i,fact=1,n; 
-  Scanner in=new Scanner(System.in);
-  int n=in.nextInt();
-  for(i=1;i<=n;i++)
+  int i,fact=1,number=5; 
+  //Scanner in=new Scanner(System.in);
+  //int n=in.nextInt();
+  for(i=1;i<=number;i++)
   {
       fact=fact*i;    
   }    
