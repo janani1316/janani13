267215cf602e28c583df952955ff542fae8c1cc4
janani1316 <jananirajadurai1397@gmail.com>
2017-07-18 09:44:17 +0530

    Create input.java

---
+++ input.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class input{
+	public static void main(String[] args)
+	{
+	int n,s=0;
+	Scanner in=new Scanner(System.in);
+	System.out.println("Enter the values");
+	n=in.nextInt();
+	for(int i=1;i<=n;i++)
+	{
+	s=s+i;
+	}
+	System.out.println(s);
+	}
+	}
